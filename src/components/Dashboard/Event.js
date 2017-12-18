a552697d406c1e7d68e386c2beb25069df098f55
Krasimir Tsonev <krasimir@outset.ws>
2017-12-18 15:46:45 +0200

    Small cleanup

--- src/components/Dashboard/Event.js
+++ src/components/Dashboard/Event.js
@@ -1,9 +1,19 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import PropTypes from 'prop-types';
 import Tree from '../Tree';
 
 export default function Event({ event }) {
-  return <Tree data={ event } />;
+  const {
+    kuker,
+    id,
+    time,
+    stateMutation,
+    mutationPaths,
+    ...rest
+  } = event;
+
+  return <Tree data={ rest } />;
 };
 
 Event.propTypes = {
