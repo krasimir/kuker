029306556560ebe6888dbd455baead10e0dd9886
Krasimir Tsonev <krasimir@outset.ws>
2018-01-03 22:45:32 +0200

    Clearing the filter

--- src/components/Dashboard/QuickFilter.js
+++ src/components/Dashboard/QuickFilter.js
@@ -2,17 +2,36 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'stent/lib/react';
 
-function QuickFilter({ whichOne, updateQuickFilters }) {
-  const handler = e => {
-    updateQuickFilters(whichOne, e.target.value);
-  };
+class QuickFilter extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className='filter'>
-      <i className='fa fa-filter'></i>
-      <input type='text' placeholder='filter' onChange={ handler }/>
-    </div>
-  );
+    this._onFieldChange = this._onFieldChange.bind(this);
+    this._clearFilter = this._clearFilter.bind(this);
+    this.state = { text: '' };
+  }
+  _onFieldChange(e) {
+    const { whichOne, updateQuickFilters } = this.props;
+    const text = e.target.value;
+
+    updateQuickFilters(whichOne, text);
+    this.setState({ text });
+  }
+  _clearFilter() {
+    this.setState({ text: '' }, () => {
+      this.props.updateQuickFilters(this.props.whichOne, '');
+    });
+  }
+  render() {
+    return (
+      <div className='filter'>
+        <i className='fa fa-filter'></i>
+        <input type='text' placeholder='filter' onChange={ this._onFieldChange } value={ this.state.text }/>
+        { this.state.text !== '' && <a className='filterClear' onClick={ this._clearFilter }>
+          <i className='fa fa-times'></i> clear</a> }
+      </div>
+    );
+  }
 };
 
 QuickFilter.propTypes = {
